3009d1625296cbc35c7411d70a532c70542423cc
JoaoVCMatias <joaovcmatias@gmail.com>
2023-06-17 22:10:21 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : Controller
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        public void AdicionaFilme(Filme filme) 
+        {
+            filmes.Add(filme);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FilmesApi/Model/Filme.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Model
+{
+    public class Filme 
+    {
+        public string Titulo { get; set; }
+        public string Gereno { get; set; }
+        public int Duracao { get; set; }
+    }
+}
