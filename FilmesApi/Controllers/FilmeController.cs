274d2e287ffc46a7de579e00dcd1480ab7e4a8bd
JoaoVCMatias <joaovcmatias@gmail.com>
2023-06-18 22:36:40 -0300

    feat: Padronizado retorno para cadastro e busca

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -11,11 +11,13 @@ namespace FilmesApi.Controllers
         private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody]Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
             filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperaFilmesPorID), new { id = filme.Id },
+                filme);
+
         }
         [HttpGet]
         public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
@@ -23,9 +25,12 @@ namespace FilmesApi.Controllers
             return filmes.Skip(skip).Take(take);
         }
         [HttpGet("{id}")]
-        public Filme? RecuperaFilmesPorID(int id)
+        public IActionResult RecuperaFilmesPorID(int id)
         {
-            return filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme? filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null) return NotFound();
+
+            return Ok(filme);
         }
     }
 }
