b164d9478cb635c30d7802eec49165065cff1ec2
JoaoVCMatias <joaovcmatias@gmail.com>
2023-06-18 23:07:30 -0300

    Adicionado um novo arquivo no gitignore

--- FilmesApi/Model/Filme.cs
+++ FilmesApi/Model/Filme.cs
@@ -5,12 +5,14 @@ namespace FilmesApi.Model
 {
     public class Filme 
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
         [Required(ErrorMessage = "O titulo do filme é obrigatório")]
-        public string Titulo { get; set; }
+        public string? Titulo { get; set; }
         [Required(ErrorMessage = "O genero do filme é obrigatório")]
         [MaxLength(50, ErrorMessage = "O tamnho do genero não pode exeder 50 caracteres")]
-        public string Genero { get; set; }
+        public string? Genero { get; set; }
         [Required(ErrorMessage = "O duração do filme é obrigatório")]
         [Range(70,600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
         public int Duracao { get; set; }
